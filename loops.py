479b740c7616c13e540d43dd4588646aa0be7e08
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-06-01 22:38:44 +0530

    updated

---
+++ loops.py
@@ -0,0 +1,8 @@
+i = 1
+while i < 11:
+    print(i)
+    if i == 5:
+        print("drungu")
+    print("done")
+    i += 1
+print("done")
--- operators.py
+++ operators.py
@@ -7,5 +7,21 @@
 #3. logical operators (or, and, not)
 #4. bitwise operators(&, |, ^, ~, <<, >>): works on binary numbers. 
 #5. assignment operators(=, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, ~=, <<=, >>=)
-#6. membership operators
-#7. identity operators
+#6. membership operators(in, not in)
+#membership operators are used to test if a sequence is presented in an object.
+#7. identity operators(is, is not)
+#identity operators are used to compare the objects, not if they are equal, but if they are actually the same object, with the same memory location.
+
+a= 10
+b=10
+print(a is b)
+
+x="hello"
+print("h" in x)
+x=[1,2,3,4,5]
+print(1 in x)
+
+i=1
+
+
+
